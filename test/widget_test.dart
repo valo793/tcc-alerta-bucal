27cf2d09ef2869517d5de41be8835742b16533b8
Leonardo <leonardoramosvallim@gmail.com>
2025-04-13 22:09:06 -0300

    eh isso dai porra

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,6 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:camera/camera.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:tcc_alertabucal/main.dart';

054480b1e286246d530cc89e02390ba01c7efa31
Leonardo <leonardoramosvallim@gmail.com>
2025-04-13 22:17:34 -0300

    pequenas mudancas na main lol

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,9 +6,7 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:camera/camera.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:tcc_alertabucal/main.dart';
 
 void main() {
